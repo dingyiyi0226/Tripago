5efb4d779835231bdcab6ab6c0bcfd308d832117
Huan-Ting,Chen <dingyiyi0226@gmail.com>
2020-12-09 15:47:20 +0800

    add imageuploader

---
+++ src/imageUploader.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import ImageUploadField from "react-images-upload"
+
+
+class ImageUploader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { pictures: [] }; // type: [File,]
+  }
+
+  onDrop = (pictureFiles, pictureDataURLs) => {
+    this.setState(state => ({
+      pictures: state.pictures.concat(pictureFiles)
+    }));
+  }
+
+  render() {
+    return (
+      <ImageUploadField
+        withPreview={true}
+        buttonText="Choose images"
+        label="Max file size = 5mb"
+        onChange={this.onDrop}
+        imgExtension={[".jpg", ".gif", ".png"]}
+      />
+    )
+  }
+  
+}
+
+export default ImageUploader
