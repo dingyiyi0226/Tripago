0a564eb563cd908b9b7d2dd15af181962a0a3292
Huan-Ting,Chen <dingyiyi0226@gmail.com>
2020-12-17 14:34:01 +0800

    fix url

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -6,14 +6,14 @@ class Header extends Component {
   render () {
     return (
       <nav className="navbar navbar-expand navbar-dark bg-primary">
-        <a className="navbar-brand" href="/">Tripago
+        <a className="navbar-brand" href={process.env.PUBLIC_URL}>Tripago
         </a>
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
-            <a className="nav-link" href="/">Home</a>
+            <a className="nav-link" href={process.env.PUBLIC_URL}>Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/">yeah</a>
+            <a className="nav-link" href={process.env.PUBLIC_URL}>yeah</a>
           </li>
         </ul>
       </nav>
