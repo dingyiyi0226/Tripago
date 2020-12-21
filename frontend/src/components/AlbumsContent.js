596cf43c9c25f9131da3ae838f26a14d411c2e5a
Huan-Ting,Chen <dingyiyi0226@gmail.com>
2020-12-22 01:32:19 +0800

    update albums page

--- frontend/src/components/AlbumsContent.js
+++ frontend/src/components/AlbumsContent.js
@@ -1,23 +1,47 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
-import { Card } from 'react-bootstrap'
+import { Card, Container, Row, Col } from 'react-bootstrap'
 
+import './component.css'
 import testpic from './testpic.png'
+import testpic2 from './testpic2.png'
 
 class AlbumsContent extends Component {
-  
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      'albumList': ['mytrip1', 'mytrip2', 'mytrip3', 'mytrip4', 'mytrip5']
+    }
+  }
+
   render () {
     return (
       <div className="p-4">
-        <Card as={NavLink} to="albums/mytrip1" style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={testpic} />
-          <Card.Body>
-            <Card.Title>MyTrip1</Card.Title>
-            <Card.Text>
-              This is MyTrip1 description. This is MyTrip1 description. This is MyTrip1 description.
-            </Card.Text>
-          </Card.Body>
-        </Card>
+        <Container>
+          <Row xs={1} sm={2} md={3} lg={4}>
+            <Col className="p-3">
+              <Card>
+                <Card.Img variant="top" src={ testpic } />
+                <Card.Body>
+                  <Card.Title>Click to Add</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            { this.state.albumList.map(album =>
+                <Col className="p-3">
+                  <Card as={NavLink} to={`albums/${album}`}>
+                    <Card.Img variant="top" src={testpic2} />
+                    <Card.Body>
+                      <Card.Title>{`${album}`}</Card.Title>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              )
+            }
+          </Row>
+        </Container>
+        
 
       </div>
     )
