32e97353a63d83eef361b687d5572ca5e8c279ba
Huan-Ting,Chen <dingyiyi0226@gmail.com>
2020-12-30 22:53:47 +0800

    store photoUrl to database

---
+++ backend/photoProcessing.js
@@ -0,0 +1,50 @@
+import { Storage } from '@google-cloud/storage'
+import { Firestore } from '@google-cloud/firestore'
+
+const cloudStorage = new Storage()
+const cloudBucket = cloudStorage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
+
+const firestore = new Firestore();
+
+async function photoProcessing(file, user, album) {
+
+  let photoUrl = ''
+  let photoLoc = {}
+
+  const upload2Storage = new Promise((resolve, reject) => {
+    const blob = cloudBucket.file(`${user}/${album}/${file.originalname}`);
+    const blobStream = blob.createWriteStream({
+      resumable: false,
+    });
+
+    blobStream.on('error', err => {
+      next(err);
+    });
+
+    blobStream.on('finish', (response) => {
+      photoUrl = `https://${cloudBucket.name}.storage.googleapis.com/${blob.name}`;
+      resolve(response)
+    });
+    blobStream.end(file.buffer);
+  })
+
+  const parsePhoto = async () => {
+    photoLoc = {lat: 25.2, lng: 121.3}
+  }
+
+  const upload2Database = async () => {
+    const photoDoc = firestore.collection(`users/${user}/albums/${album}/photos/`).doc()
+    let res = await photoDoc.set({
+      url: photoUrl,
+      location: new Firestore.GeoPoint(photoLoc.lat, photoLoc.lng)
+    })
+  }
+
+  await upload2Storage
+  await parsePhoto()
+  await upload2Database()
+
+  console.log(`finish processing ${file.originalname}`)
+}
+
+export default photoProcessing
--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,42 @@
-import fs from 'fs'
 import cors from 'cors'
 import express from 'express'
 import multer from 'multer'
 import { Storage } from '@google-cloud/storage'
+import { Firestore } from '@google-cloud/firestore'
+
+import photoProcessing from './photoProcessing.js'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-// Store images to disk, cannot deploy to app engine
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-
-//     const { album } = JSON.parse(JSON.stringify(req.body))
-//   // console.log(album)
-//     const path = `albums/${album}`
-
-//     fs.mkdirSync(path, { recursive: true })
-//     cb(null, path)
-//   },
-//   filename: (req, file, cb) => {
-//     const fileName = file.originalname
-//     cb(null, fileName)
-//   }
-// })
-
 const storage = multer.memoryStorage()
-
 const cloudStorage = new Storage()
 const cloudBucket = cloudStorage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
+const firestore = new Firestore();
 
 const USER = 'ethia_polis'
 
 const upload = multer({storage: storage})
 const MAX_FILE = 12
+
+/**
+  DB Schema (temp version)
+  users/
+    user1:
+      description
+      albums/
+        album1:
+          description
+          photos/
+            photo1:
+              url
+              geopoint
+            photo2:
+        album2:
+    user2:
+ */
+
 
 app.post('/photos', upload.array('photos', MAX_FILE), async (req, res, next) => {
   if (!req.files.length){
@@ -44,32 +47,10 @@ app.post('/photos', upload.array('photos', MAX_FILE), async (req, res, next) => 
   // console.log('success')
   const { album } = JSON.parse(JSON.stringify(req.body))
 
-  let publicUrls = []
-  let promises = []
-
-  req.files.forEach(file => {
-    const promise = new Promise((resolve, reject) => {
-      const blob = cloudBucket.file(`${USER}/${album}/${file.originalname}`);
-      const blobStream = blob.createWriteStream({
-        resumable: false,
-      });
-
-      blobStream.on('error', err => {
-        next(err);
-      });
-
-      blobStream.on('finish', (response) => {
-        const publicUrl = `https://${cloudBucket.name}.storage.googleapis.com/${blob.name}`;
-        publicUrls.push(publicUrl)
-        resolve(response)
-      });
-      blobStream.end(file.buffer);
-    })
-    promises.push(promise)
-  })
-  await Promise.all(promises)
-  console.log(publicUrls)
-  res.status(200).send(publicUrls);
+  for (let file of req.files) {
+    await photoProcessing(file, USER, album)  // Should the server response to client immediatly or await photo upload to db or not ?
+  }
+  res.status(200).send();
 })
 
 // Debug function
