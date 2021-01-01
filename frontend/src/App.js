0d09f83a8afd2b00fc199af852819c26c1d3d644
m24639297 <m24639297@gmail.com>
2021-01-02 03:28:59 +0800

    Brief: add context for login
    (1) Add a react-context (LoginContext.js) to
        handle logged-in and unlogged-in view of
        the website. (Added in App.js)
    (2) Rewrite Tripago.js as a function componet
        rather than a class-based one, so that
        context hooks can work normally
    (3) Modify login/reg/logout button in Header
    (**4**)
        If isLogin==false in context:
            Redirect all routes except ./register
            to ./login.
        else:
            Everything works normally as before,
            with the "login/register" button replaced
            by a "logout", which redirects users to
            the login page.
    (5) TODO in next commit (hopefully)
        Set context as register/login/logout clicked
        (So they behave wierd now, sorry QQ)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
+import { useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-import Tripago from './containers/Tripago.js'
+import Tripago from './containers/Tripago.js';
+import LoginContext from './LoginContext.js';
 
 function App() {
+	const [isLogin, setIsLogin] = useState(true);
+	const value = { isLogin, setIsLogin };
+
   return (
     <div className="App">
+    <LoginContext.Provider value={value}>
       <Tripago />
+    </LoginContext.Provider>
     </div>
   );
 }
--- frontend/src/containers/Tripago.js
+++ frontend/src/containers/Tripago.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useContext } from 'react'  //import useContext preparing for auto-redirection to login page
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 
 import Header from '../components/Header.js'
@@ -7,38 +7,55 @@ import Albums from '../components/Albums.js'
 import Album from '../components/Album'
 import Login from '../components/auth/Login'
 import Registration from '../components/auth/Registration'
+import LoginContext from '../LoginContext.js'
 
-class Tripago extends Component {
-
-  render() {
+const Tripago = () => {
+  const {isLogin, setIsLogin} = useContext(LoginContext);
+  if (!isLogin) {
     return (
       <BrowserRouter basename={ process.env.PUBLIC_URL }>
         <Header />
         <Switch>
-          <Route exact path="/">
-            <Redirect to="/home" />
-          </Route>
-          <Route path="/home">
-            <Home />
-          </Route>
-          <Route exact path="/albums">
-            <Albums />
-          </Route>
-          <Route path="/albums/:id">
-            <Album />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Registration />
-          </Route>
-        </Switch>
-
+        <Route path="/register">
+          <Registration />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Redirect to="/login" />
+        </Route>
+      </Switch>
       </BrowserRouter>
-
-    )
+      );
   }
+  return (
+    <BrowserRouter basename={ process.env.PUBLIC_URL }>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/albums">
+          <Albums />
+        </Route>
+        <Route path="/albums/:id">
+          <Album />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Registration />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+
+  )
 }
+
 
 export default Tripago
