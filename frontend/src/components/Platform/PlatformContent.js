8ac8683e2f3378227d1db689390e62a53276342f
Huan-Ting,Chen <dingyiyi0226@gmail.com>
2021-01-05 12:34:20 +0800

    add query frontend

--- frontend/src/components/Platform/PlatformContent.js
+++ frontend/src/components/Platform/PlatformContent.js
@@ -1,26 +1,37 @@
-import React, { Component } from 'react'
-
+import React, { useState, useEffect } from 'react'
+import { useLocation} from 'react-router-dom'
 import PlatformCard from './PlatformCard.js'
 import './Platform.css'
 
-
-class Content extends Component {
-
-  render() {
-    return (
-      <div className="h-100">
-        <p className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          Platform content
-        </p>
-        <div className="platform">
-          <PlatformCard />
-          <PlatformCard />
-          <PlatformCard />
-
-        </div>
-      </div>
-    )
-  }
+function useQuery() {
+  const params = new URLSearchParams(useLocation().search)
+  return params.get('region')
 }
 
-export default Content
+function PlatformContent(props) {
+  
+  let region = useQuery()
+  useEffect(() => {
+    console.log('useeffect', region)
+    // TODO: Fetch data from backend
+
+  })
+
+
+  return (
+    <div className="h-100">
+      <p className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        Region - { region || 'All Region' }
+      </p>
+      <div className="platform">
+        <PlatformCard />
+        <PlatformCard />
+        <PlatformCard />
+
+      </div>
+    </div>
+  )
+  
+}
+
+export default PlatformContent
--- frontend/src/components/Platform/PlatformSidebar.js
+++ frontend/src/components/Platform/PlatformSidebar.js
@@ -1,31 +1,50 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 import { Image, Form, InputGroup } from 'react-bootstrap'
 
 import './Platform.css'
 import searchImage from './search-outline.svg'
 
 class PlatformSidebar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      region: '',
+      options: ['Taiwan', 'Japan', 'Tainan', 'Taichung']
+    }
+  }
 
-  onClick = () => {
-    console.log('onclick')
+  onInputRegion = (e) => {
+    this.setState({region: e.target.value})
+  }
+
+  onSearch = () => {
+    this.setState({region: ''})
+  }
+
+  onSelect = (e) => {
+    this.setState({region: e.target.id})
   }
  
   render() {
+    const { region } = this.state
     return (
       <div className="platform-sidebar position-sticky">
         <p className="pt-3 pb-2 mb-3 border-bottom">Filter</p>
         <Form>
           <Form.Group controlId="region">
-            <Form.Label>Region</Form.Label>
+            <Form.Label>Region Search</Form.Label>
             <InputGroup size="sm">
-              <Form.Control type="text" placeholder="Taiwan"/>
-              <InputGroup.Append onClick={this.onClick}>
+              <Form.Control type="text" value={region} placeholder="Region" onChange={this.onInputRegion}/>
+              <InputGroup.Append as={NavLink} to={{pathname: '/platform', search: `?region=${region}`}} onClick={this.onSearch}>
                 <Image src={searchImage}></Image>
               </InputGroup.Append>
             </InputGroup>
           </Form.Group>
-          <Form.Check custom type="radio" id="taiwan" name="region-radio" label="Taiwan" />
-          <Form.Check custom type="radio" id="japan" name="region-radio" label="Japan" />
+          { this.state.options.map( option => (
+              <Form.Check custom type="radio" name="region-radio" id={option} label={option} checked={region===option} onChange={this.onSelect} key={option}/>
+            ))
+          }
         </Form>
       </div>
     )
