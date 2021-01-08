2c4469f36416cac964f9cc818c9e47e0d8e43d00
Huan-Ting,Chen <dingyiyi0226@gmail.com>
2021-01-08 15:23:35 +0800

    update profile sidebar

--- frontend/src/components/ProfileSidebar.js
+++ frontend/src/components/ProfileSidebar.js
@@ -5,16 +5,30 @@ import './component.css'
 import testpic from './testpic.png'
 
 class ProfileSidebar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userName: 'Ethia Polis',
+      userDescription: 'SuperStar',
+      userPhoto: testpic
+    }
+  }
+
+  componentDidMount() {
+    const getUserInfo = async () => {
+      console.log('getUserInfo')
+    }
+
+    getUserInfo()
+  }
   
   render () {
     return (
       <div className="p-3">
-        <Image src={testpic} fluid rounded className="my-3"/>
+        <Image src={this.state.userPhoto} fluid rounded className="my-3"/>
         <div className="profile__text px-4">
-          <h3 className="py-1">Ethia Polis</h3>
-          <h6>#SuperStar</h6>
-          <h6>#Star4Real</h6>
-          <h6>#StarFish</h6>
+          <h3 className="py-1">{this.state.userName}</h3>
+          <h6>{this.state.userDescription}</h6>
         </div>
       </div>
     )
